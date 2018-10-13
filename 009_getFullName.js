7bfaf64f4b6dac826be91b85f5d8ca8f2390070e
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-10-13 16:27:10 -0700

    009_getFullName

---
+++ 009_getFullName.js
@@ -0,0 +1,36 @@
+//Astra Rai Daniels 
+//October 13, 2018
+//4:00 PM
+
+//Instructions:
+
+/*
+Write a function called "getFullName".
+Given a first and a last name, "getFullName" returns a single string with the given first and last names separated by a single space.
+var output = getFullName('Joe', 'Smith');
+console.log(output); // --> 'Joe Smith'
+*/
+
+//My Thoughts and Notes:
+//The concat() method concatenates the string arguments to the calling string and returns a new string.
+
+//Follow Example, Resource MDN: 
+
+/*var str1 = 'Hello';
+var str2 = 'World';
+
+console.log(str1.concat(' ', str2));
+// expected output: "Hello World"
+
+console.log(str2.concat(', ', str1));
+// expected output: "World, Hello"
+*/
+// expected output: "Hello World"
+
+
+function getFullName(firstName, lastName) {
+  return ;
+}
+
+var output = getFullName('Joe', 'Smith');
+console.log(output);
