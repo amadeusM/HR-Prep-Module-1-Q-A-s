d7c49ae245b800a37f3bf088791a12cea9d4e492
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-10-13 06:15:13 -0700

    043_computeAreaOfARectangle

---
+++ 043_computeAreaOfARectangle.js
@@ -0,0 +1,23 @@
+//Astra Rai Daniels 
+//October 13, 2018
+//6:08 AM
+
+//Instructions
+
+/*
+Write a function called "computeAreaOfARectangle".
+Given the length and width of a rectangle, "computeAreaOfARectangle" returns its area.
+var output = computeAreaOfARectangle(4, 8);
+console.log(output); // --> 32
+*/
+
+//My Thoughts
+//Use the following formula to calculate area of recetanlge
+//Area = Length * width
+
+function computeAreaOfARectangle(length, width) {
+  return length * width;
+}
+
+var output = computeAreaOfARectangle(4, 8);
+console.log(output);
