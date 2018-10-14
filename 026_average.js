8b8e9d5ea12ac1a1a982c257e350fd6843dc7e9b
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-10-14 13:17:14 -0700

    026_average

---
+++ 026_average.js
@@ -0,0 +1,26 @@
+//Astra Rai Daniels 
+//October 14, 2018
+//1:14 PM
+
+
+
+//Instructions:
+
+/*
+Write a function called "average".
+Given two numbers, "average" returns their average.
+var output = average(4, 6);
+console.log(output); // --> 5
+*/
+
+//My Thoughts:
+
+//Find average: (a + b)/2 
+
+
+function average(num1, num2) {
+  return (num1 + num2)/2;
+}
+
+var output = average(4,6);
+console.log(output); // -->5
