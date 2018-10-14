3439163162d24d582090a99b85365a8de9155df0
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-10-14 13:26:34 -0700

    027_computeAreaOfATriangle

---
+++ 027_computeAreaOfATriangle.js
@@ -0,0 +1,22 @@
+//Astra Rai Daniels 
+//October 14, 2018
+//1:21 PM
+
+//Instructions
+/*
+Write a function called "computeAreaOfATriangle".
+Given the base and height of a triangle, "computeAreaOfATriangle" returns its area.
+var output = computeAreaOfATriangle(4, 6);
+console.log(output); // --> 12
+*/
+
+//My Thoughts:
+
+//Formula forArea of Triangle: 1/2 * base * height
+
+
+function computeAreaOfATriangle(base, height) {
+  return (base * height) * 0.5;
+}
+var output = computeAreaOfATriangle(4,6);
+console.log(output); //--> 12,
