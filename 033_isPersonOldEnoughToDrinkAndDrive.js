3b5f38475dae45cbcc7d543ba4338c7a2bbc69a7
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-10-15 22:04:01 -0700

    033_isPersonOldEnoughToDrinkAndDrive

---
+++ 033_isPersonOldEnoughToDrinkAndDrive.js
@@ -0,0 +1,34 @@
+//Astra Rai Daniels 
+//October 14, 2018
+//9:58PM
+
+
+//Instructions:
+
+/*
+Write a function called "isPersonOldEnoughToDrinkAndDrive".
+Given a "person" object, that contains an "age" property, "isPersonOldEnoughToDrinkAndDrive" returns whether the given person is old enough to legally drink and drive in the United States.
+Notes:
+* The legal drinking age in the United States is 21.
+* The legal driving age in the United States is 16.
+* It is always illegal to drink and drive in the United States.
+var obj = {
+  age: 45
+};
+var output = isPersonOldEnoughToDrinkAndDrive(obj);
+console.log(output); // --> false
+*/
+
+//My Thought Process:
+//Statement should return false regardless of age given.
+
+var obj = {
+  age: 45
+};
+
+function isPersonOldEnoughToDrinkAndDrive(person) {
+  return false;
+}
+
+var output = isPersonOldEnoughToDrinkAndDrive(obj);
+console.log(output);
