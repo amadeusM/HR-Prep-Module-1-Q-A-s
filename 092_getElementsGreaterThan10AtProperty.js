569e54f446daaa96424bf0872152d40390ec8656
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-11-03 19:09:48 -0700

    092_getElementsGreaterThan10AtProperty

---
+++ 092_getElementsGreaterThan10AtProperty.js
@@ -0,0 +1,36 @@
+/*
+Write a function called "getElementsGreaterThan10AtProperty".
+
+Given an object and a key, "getElementsGreaterThan10AtProperty" returns an array containing the elements within the array, located at the given key, that are greater than 10.
+
+Notes:
+* If the array is empty, it should return an empty array.
+* If the array contains no elements greater than 10, it should return an empty array.
+* If the property at the given key is not an array, it should return an empty array.
+* If there is no property at the key, it should return an empty array. 
+
+var obj = {
+  key: [1, 20, 30]
+};
+var output = getElementsGreaterThan10AtProperty(obj, 'key');
+console.log(output); // --> [20, 30]
+
+*/
+
+var obj = {
+  key: [1, 20, 30]
+};
+
+function getElementsGreaterThan10AtProperty(obj, key) {
+  var rtnArray = [];
+  
+  for(var element in obj[key]){
+    if(obj[key][element] > 10){
+      rtnArray.push(obj[key][element]);
+    }
+  }
+  return rtnArray;
+}
+
+var output = getElementsGreaterThan10AtProperty(obj,'key');
+console.log(output);
