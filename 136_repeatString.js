a4ea1ed08f7107fd1dae4392ac2eb7134b0c9976
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-12-09 16:10:35 -0600

    136_repeatString ?

---
+++ 136_repeatString.js
@@ -0,0 +1,21 @@
+/*
+Write a function called "repeatString".
+
+Given a string and a number, "repeatString" returns the given string repeated the given number of times.
+
+var output = repeatString('code', 3);
+console.log(output); // --> 'codecodecode'
+*/
+
+function repeatString(string, num) {
+  // your code here
+  var stringOutput = '';
+  for(var i = 0; i < num; i++){
+    stringOutput += string;
+  }
+  return stringOutput;
+}
+
+
+var output = repeatString('code', 3);
+console.log(output); // --> 'codecodecode'
