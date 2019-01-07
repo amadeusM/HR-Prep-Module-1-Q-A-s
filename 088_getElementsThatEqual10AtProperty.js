a22dd43e0e1dacd829cc8ffe04275814aa490918
amadeusM <44307034+amadeusM@users.noreply.github.com>
2019-01-07 10:23:11 -0800

    Update 088_getElementsThatEqual10AtProperty.js

--- 088_getElementsThatEqual10AtProperty.js
+++ 088_getElementsThatEqual10AtProperty.js
@@ -16,7 +16,19 @@ var output = getElementsThatEqual10AtProperty(obj, 'key');
 console.log(output); // --> [10, 10]
 */
 
-function getElementsThatEqual10AtProperty(obj, key) {
-  // your code here
-
+function getElementsGreaterThan10AtProperty(obj, key) {
+  var arr = [];
+  for (var elem in obj[key]) {
+    if (obj[key][elem] > 10) {
+      arr.push(obj[key][elem]);
+    }
+  }
+  return arr;
 }
+
+var obj = {
+  key: [1, 20, 30]
+};
+
+var output = getElementsGreaterThan10AtProperty(obj, 'key');
+console.log(output); // --> [20, 30]
